d338c0a652f74bcb5b2d92fc1d508dea303a6b0a
Ivan Bondarenko <krenkroyt@gmail.com>
2024-02-06 18:07:59 +0400

    added: edit task feature

--- todo/urls.py
+++ todo/urls.py
@@ -2,7 +2,12 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    # Add task
     path("addTask/", views.add_task, name="addTask"),
+    # Mark as done
     path("mark_as_done/<int:pk>", views.mark_as_done, name="mark_as_done"),
+    # Mark as undone
     path("mark_as_undone/<int:pk>", views.mark_as_undone, name="mark_as_undone"),
+    # EDIT Feature
+    path("edit_task/<int:pk>", views.edit_task, name="edit_task"),
 ]
