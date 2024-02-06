59a374ab4e278793c6b5933549379676251edf1e
Ivan Bondarenko <krenkroyt@gmail.com>
2024-02-06 17:30:21 +0400

    added: mark as done

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("addTask/", views.add_task, name="addTask"),
+    path("mark_as_done/<int:pk>", views.mark_as_done, name="mark_as_done"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from todo.models import Task
 
@@ -8,3 +8,12 @@ def add_task(request):
     task = request.POST["task"]
     Task.objects.create(task=task)
     return redirect("home")
+
+
+def mark_as_done(request, pk):
+
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect("home")
+    # return HttpResponse(pk)
