7cb8ff1a12173b5df43e2521d54969e8e54098f5
Ivan Bondarenko <krenkroyt@gmail.com>
2024-02-06 18:20:35 +0400

    added: delete task

--- todo/urls.py
+++ todo/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path("mark_as_done/<int:pk>", views.mark_as_done, name="mark_as_done"),
     # Mark as undone
     path("mark_as_undone/<int:pk>", views.mark_as_undone, name="mark_as_undone"),
-    # EDIT Feature
+    # Edit Feature
     path("edit_task/<int:pk>", views.edit_task, name="edit_task"),
+    # Delete Feature
+    path("delete_task/<int:pk>", views.delete_task, name="delete_task"),
 ]
